e67a8b306b93b6606fc3b5b01fb98f92f655f70f
Aayush7475 <vatsxaayush2006@gmail.com>
2026-07-15 12:59:34 +0530

    Searching Matrix II Ques

--- BinarySearch/2D-Arrays.cpp
+++ BinarySearch/2D-Arrays.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std ; 
 
+// Finding the row number containing maximum number of 1s
 int lowerBound(vector<int>& nums , int x ){
     int n = nums.size() ;
     int low = 0 ;
@@ -37,6 +38,7 @@ int rowWithMax_1s(vector<vector<int>>& matrix , int n , int m ){
     return index ;
 }
 
+// Finding the target value in the matrix having sorted rows 
 bool searchMatrix(vector<vector<int>>& matrix , int target){
     if(matrix.size() == 0) return false ;
 
@@ -61,6 +63,23 @@ bool searchMatrix(vector<vector<int>>& matrix , int target){
     return false ;
 }
 
+// Finding the target value in the matrix having sorted rows nd columns
+bool SearchMatrix(vector<vector<int>>& matrix , int target){
+     int n = matrix.size() ;
+     int m = matrix[0].size() ;
+
+     int row = 0 ;
+     int column = m-1 ;
+
+     while(row < n && column >= 0){
+        if(matrix[row][column] == target) return true ;
+        if(matrix[row][column] > target) column-- ;
+        else row++ ;
+     } 
+     return false ;  
+}
+
+// 
 
 int main(){
     int n;
@@ -83,9 +102,9 @@ int main(){
     }
     cout << endl ;
     
-    // int target ;         // Searching element in the matrix 
-    // cout << "Enter the number of be searched in the matrix : " ;
-    // cin >> target ;
+    int target ;         // Searching element in the matrix 
+    cout << "Enter the number of be searched in the matrix : " ;
+    cin >> target ;
 
     // cout << "Row with maximum number of ones is : " << rowWithMax_1s(matrix , n , m) ;
 
@@ -97,5 +116,14 @@ int main(){
     //     cout << "Searched element is not in the matrix " ;
     // }
 
+    bool result = SearchMatrix(matrix , target) ;
+    if(result == true ){
+        cout << "Searched element exists in the matrix " ;
+    }
+    else{
+        cout << "Searched element is not in the matrix " ;
+    }
+
+    
     return 0 ;
 }
