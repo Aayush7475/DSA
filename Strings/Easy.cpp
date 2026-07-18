df33f8e86a081008ac591389d2f62adc5b8bbb6f
Aayush7475 <vatsxaayush2006@gmail.com>
2026-07-18 21:54:11 +0530

    Largest common prefix Ques

--- Strings/Easy.cpp
+++ Strings/Easy.cpp
@@ -44,16 +44,67 @@ string reverseWords(string s){
     return ans ;
 }
 
+// Returning the largest odd number(sub-string) 
+string largestOddNumber(string s){
+    int n = s.length() ;
+    int index = -1 ;
+
+    for(int i = n-1 ; i >= 0 ; i--){
+        if((s[i] - '0') % 2 == 1){
+            index = i ;
+            break ;
+        }
+    }
+    int i = 0 ;
+    while(i < index && s[i] == '0') i++ ;
+
+    return s.substr(i , index - i + 1) ;
+}
+
+// Returning the largest common prefix 
+string largestCommonPrefix(vector<string>& s){
+    if(s.empty()) return "";
+    sort(s.begin() , s.end()) ;
+
+    string first = s[0] ;
+    string last = s[s.size() - 1] ;
+
+    string ans = "" ;
+
+    int minLength = min(first.size() , last.size()) ;
+
+    for(int i = 0 ; i < minLength ; i++){
+        if(first[i] != last[i]){
+            break ;
+        }
+        ans += first[i] ;
+    }
+    return ans ;
+}
+
+
 int main(){
     string s ;
     cout << "Enter the string  : " ;
     getline(cin , s) ;
 
+    // int n;        // Taking number of strings and strings also
+    // cout << "Enter number of strings: ";
+    // cin >> n;
+    // vector<string> s(n);
+    // cout << "Enter the strings:\n";
+    // for(int i = 0; i < n; i++){
+    //     cin >> s[i];
+    // }
 
 
     // cout << "After removing the outermost parenthesis given parenthesis is : " << removeOuterParentheses(s) ;
 
     // cout << "String in the reverse order is : " << reverseWords(s) ;
 
+    // cout << "Largest odd number possible is : " << largestOddNumber(s) ;
+
+    // cout << "Largest common string : " << largestCommonPrefix(s) ;
+    
     return 0 ;
 }
