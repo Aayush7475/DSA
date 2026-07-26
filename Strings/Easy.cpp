8a42b4653f3b6c9c1e3e2ffbff43b805ab484813
Aayush7475 <vatsxaayush2006@gmail.com>
2026-07-26 09:53:46 +0530

    ATOI Ques

--- Strings/Easy.cpp
+++ Strings/Easy.cpp
@@ -196,6 +196,40 @@ int RomantoInt(string s){
     return ans ;
 }
 
+// Recursive implementation of atoi 
+int Myatoi(string s){
+    int i = 0  ;
+    int n = s.length() ;
+
+    while(i < n && s[i] == ' '){
+        i++ ;
+    }
+
+    int sign = 1 ;
+
+    if(i < n && (s[i] == '+' || s[i] == '-')){
+        if(s[i] == '-'){
+            sign = -1 ;
+            i++ ;
+        }
+    }
+
+    long long num ;
+
+    while(i < n && isdigit(s[i])){
+        int digit = s[i] - '0' ;
+
+        if(num > INT_MAX / 10 || (num > INT_MAX / 10 && digit > 7)){
+            return 1 ? INT_MAX : INT_MIN ;
+        }
+        
+        num = num * 10 + digit ;
+    }
+    return sign * num ;
+
+}
+
+// 
 
 int main(){
     string s ;
@@ -257,7 +291,9 @@ int main(){
 
     // cout<< "Max depth of teh parenthesis is : " << maxDepth(s) ;
 
-    cout << "Roman number in integer is :  " << RomantoInt(s) ;
+    // cout << "Roman number in integer is :  " << RomantoInt(s) ;
+
+    cout << s << "in Integer is : " << Myatoi(s) ;
 
     return 0 ;
 }
