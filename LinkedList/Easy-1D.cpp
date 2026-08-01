4fc651bdfe6f8e7f5e97f1fbacacbcbece265cde
Aayush7475 <vatsxaayush2006@gmail.com>
2026-08-01 16:36:52 +0530

    LinkedList Started

--- LinkedList/Easy-1D.cpp
+++ LinkedList/Easy-1D.cpp
@@ -19,8 +19,42 @@ class Node {
     }
 };
 
+// Converting Array into LinkedList ->
+Node* convertArr2LL(vector<int> &nums){
+    Node* head = new Node(nums[0]) ;
+    Node* mover = head ;
+
+    for(int i = 1 ; i < nums.size() ; i++){
+        Node* temp = new Node(nums[i]) ;
+        mover -> next = temp ;
+        mover = temp ;
+    }
+    return head ;
+}
+
+
+
 int main(){
-    vector<int> arr = {2 , 5 , 8 , 7};
-    Node* y = new Node{arr[0]} ;
-    cout << y -> data ;
+    int n ;
+    cout << "Enter the number of elements in the array : "   ;
+    cin >> n ;
+
+    vector<int> nums(n) ;
+
+    cout << "Enter the elements in the array :" << endl;
+    for(int i = 0 ; i < n ; i++){
+        cin >> nums[i];
+    }
+
+    // Node* y = new Node{nums[0]} ;
+    // cout << "First element is : " ;
+    // cout << y -> data ;
+
+    Node* head = convertArr2LL(nums) ;
+    cout << "1st element in the LinkedList is : " ;
+    cout << head -> data ;
+
+    return 0 ;
+
+
 } 
