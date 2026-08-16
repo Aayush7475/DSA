782ad4ffa7bed08cb85028fe7f04ac6d1b6f9609
Aayush7475 <vatsxaayush2006@gmail.com>
2026-08-16 17:58:26 +0530

    Reversing a LL

--- LinkedList/Med-1LL.cpp
+++ LinkedList/Med-1LL.cpp
@@ -45,7 +45,17 @@ Node* convertArr2LL(vector<int> &nums){
 }
 
 // Reversing the LL ->
-
+Node* reverse(Node* head){
+    Node* temp = head ;
+    Node* prev = NULL ;
+    while(temp != NULL){
+        Node* front = temp -> next ;
+        temp -> next = prev ;
+        prev = temp ;
+        temp = front ;
+    }
+    return prev ;
+}
 
 int main(){
     int n ; 
@@ -61,6 +71,11 @@ int main(){
 
     // Converting array into LL ->
     Node* head = convertArr2LL(nums);
+
+    // Reversing a LL ->
+    // cout << "Reversed LL is : " ;
+    // head = reverse(head) ;
+    // print(head) ;
 
   
 
