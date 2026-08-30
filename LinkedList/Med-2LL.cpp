51c473ae6e3aa22706687373356050605ea0b221
Aayush7475 <vatsxaayush2006@gmail.com>
2026-08-30 13:14:18 +0530

    Deleting occurences of key in DLL

--- LinkedList/Med-2LL.cpp
+++ LinkedList/Med-2LL.cpp
@@ -5,19 +5,19 @@ class Node{
     public : 
     int data ; 
     Node* next ; 
-    Node* back ;
+    Node* prev ;
 
     public : 
-    Node(int data1 , Node* next1 , Node* back1){
+    Node(int data1 , Node* next1 , Node* prev1){
         data = data1 ;
         next = next1 ;
-        back = back1 ;
+        prev = prev1 ;
     }
     public :
     Node(int data1){
         data = data1 ;
         next = nullptr ;
-        back = nullptr ;
+        prev = nullptr ;
     }
 };
 
@@ -43,7 +43,53 @@ Node* convertArr2DLL(vector<int> &nums){
     return head ;
 }
 
-// 
-int main(){
+// Deleting all Occurences of a key in DLL ->
+Node* deleteOccurences(Node* head , int key){
+    Node* temp = head ;
+    while(temp != NULL){
+        if(temp -> data == key){
+            if(temp == head){
+                head = temp -> next ;
+            }
+            Node* nextNode = temp -> next ;
+            Node* prevNode = temp -> prev ;
 
+            if(nextNode != NULL) nextNode -> prev = prevNode ;
+            if(prevNode != NULL) prevNode -> next = nextNode ;
+            
+            free(temp) ;
+            temp = nextNode ;
+        }
+        else{
+            temp = temp -> next ;
+        }
+    }
+    return head ;
+}
+
+
+int main(){
+     int n ; 
+    cout << "Enter the number of elements in the array : ";
+    cin >> n ;
+
+    vector<int> nums(n) ;
+
+    cout << "Enter the elements in the array :" << endl;
+    for(int i = 0 ; i < n ; i++){
+        cin >> nums[i];
+    }
+
+    // Converting array into DLL ->
+    Node* head = convertArr2DLL(nums);
+
+    // Deleting all Occurences of a key in DLL ->
+    // int key ;
+    // cout << "Enter the value of key : " ;
+    // cin >> key ;
+    // head = deleteOccurences(head , key) ;
+    // print(head) ;
+
+    
+    return 0 ;
 }
