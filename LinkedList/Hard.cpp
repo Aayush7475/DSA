b728954cfd7081ebc70350e5dab169ba81b0ec00
Aayush7475 <vatsxaayush2006@gmail.com>
2026-09-05 01:00:27 +0530

    Cloning A LL with random pointers M2

--- LinkedList/Hard.cpp
+++ LinkedList/Hard.cpp
@@ -1,24 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std ; 
 
-class Node{
-    public : 
-    int data ; 
-    Node* next ; 
-    Node* prev ;
-    public : 
-    Node(int data1 , Node* next1 , Node* prev1){
-        data = data1 ;
-        next = next1 ;
-        prev = prev1 ;
-    }
-    public :
-    Node(int data1){
-        data = data1 ;
-        next = nullptr ;
-        prev = nullptr ;
-    }
-};
+// class Node{
+//     public : 
+//     int data ; 
+//     Node* next ; 
+//     Node* prev ;
+//     public : 
+//     Node(int data1 , Node* next1 , Node* prev1){
+//         data = data1 ;
+//         next = next1 ;
+//         prev = prev1 ;
+//     }
+//     public :
+//     Node(int data1){
+//         data = data1 ;
+//         next = nullptr ;
+//         prev = nullptr ;
+//     }
+// };
 
 // class Node{     // For Flattening of LL 
 //     public : 
@@ -39,21 +39,21 @@ class Node{
 //     }
 // };
 
-// class Node{       // For Cloning LL with Random & next Pointer 
-// public:
-//     // Data stored in the node
-//     int data;           
-//      // Pointer to the next node
-//     Node *next;        
-//     // Pointer to a random node in the list
-//     Node *random;       
-//     // Constructors for Node class
-//     Node() : data(0), next(nullptr), random(nullptr){}; 
-//     Node(int x) : data(x), next(nullptr), random(nullptr) {} 
-//     // Constructor with data, next, and random pointers
-//     Node(int x, Node *nextNode, Node *randomNode) :
-//             data(x), next(nextNode), random(randomNode) {}  
-// };
+class Node{     // For Cloning LL with Random & next Pointer 
+public:
+    // Data stored in the node
+    int data;           
+     // Pointer to the next node
+    Node *next;        
+    // Pointer to a random node in the list
+    Node *random;       
+    // Constructors for Node class
+    Node() : data(0), next(nullptr), random(nullptr){}; 
+    Node(int x) : data(x), next(nullptr), random(nullptr) {} 
+    // Constructor with data, next, and random pointers
+    Node(int x, Node *nextNode, Node *randomNode) :
+            data(x), next(nextNode), random(randomNode) {}  
+};
 
 
 void print(Node* head){
@@ -230,7 +230,7 @@ Node* Flattening_2(Node* head){
 
 }
 
-// Cloning List with Random pointer ->
+// Cloning List with Random pointer (Method 1) ->
 Node* copyrandomList(Node* head){
     Node* temp = head ;
     map<Node* , Node*> mpp ;
@@ -249,6 +249,41 @@ Node* copyrandomList(Node* head){
     return mpp[head] ;
 }
 
+// Cloning List with Random pointer (Method 2) ->
+Node* insertCopyNodes(Node* head){
+    Node* temp = head ;
+    while(temp != NULL){
+        Node* copyNode = new Node(temp -> data) ;
+        copyNode -> next = temp -> next ;
+        temp -> next = copyNode ;
+        temp = temp -> next -> next ;
+    }
+}
+Node* connectingRandoms(Node* head){
+    Node* temp = head ;
+    while(temp != NULL){
+        Node* copyNode = temp -> next ;
+        copyNode -> random = temp -> random -> next ;
+        temp = temp -> next -> next ;
+    }
+}
+Node* connectingNexts(Node* head){
+    Node* dummy = new Node(-1) ;
+    Node* res = dummy ;
+    Node* temp = head ;
+    while(temp != NULL){
+        res ->next = temp -> next ;
+        temp -> next = temp -> next -> next ;
+        res = res -> next ;
+        temp = temp -> next ;
+    }
+    return dummy -> next ;
+}
+Node* cloningRandomLL(Node* head){
+    insertCopyNodes(head) ;
+    connectingRandoms(head) ;
+    return connectingNexts(head) ;
+}
 
 int main(){
     int n ; 
