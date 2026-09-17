73a87941c98a839729cededda93d5b2215286270
Aayush7475 <vatsxaayush2006@gmail.com>
2026-09-17 18:27:47 +0530

    Min Sum of Lenghts

--- Arrays/Array-Med_1.cpp
+++ Arrays/Array-Med_1.cpp
@@ -256,7 +256,43 @@ int SubArraySum(vector<int> arr , int k){
     return cnt ;
 }
 
+// Two Non-Overlapping Sub Arrays each with target sum ->
+int minSumOfLengths(vector<int>& arr , int target){
+    int n = arr.size() ;
+    vector<int> best(n , INT_MAX) ;
+    int left = 0 ;
+    int sum = 0 ;
+    int ans = INT_MAX ;
 
+    for(int right = 0 ; right < n ; right++){
+        sum += arr[right] ;
+
+        while(sum > target){
+            sum -= arr[left] ;
+            left ++ ;
+        }
+
+        if(sum == target){
+            int len = right - left + 1 ;
+
+            if(left > 0 && best[left - 1] != INT_MAX){
+                ans = min(ans , len + best[left - 1]) ;
+            }
+            best[right] = len ;
+
+        }
+
+        if(right > 0){
+            best[right] = min(best[right] , best[right -1]) ;
+        }
+
+    }
+    if(ans == INT_MAX){
+        return -1 ;
+    }
+    
+    return ans ;
+}
 
 
 
@@ -339,6 +375,11 @@ int main(){
     // cin >> k ;
     // int ans = SubArraySum(arr , k );
     // cout << "Number of Sub Arrays with sum k is : " << ans;
+
+    // int target ;
+    // cout << "Enter the target element : " ;
+    // cin >> target ;
+    // cout << "Minimum sum of lengths of SubArrays is " << minSumOfLengths(arr , target);
 
     return 0 ;
 }
